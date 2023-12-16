dea043cfdad766c742114e864e5ffd60343f6a02
lakshmineha <srilaxmi550@gmail.com>
2023-12-16 14:18:53 +0530

    first commited

---
+++ hai/src/hai/Classnotes.java
@@ -0,0 +1,15 @@
+package hai;
+
+public class Classnotes {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		char ch='g';
+		String name="sree";
+		System.out.println(ch);
+		System.out.println(name);
+		//System.out.println(a);
+
+	}
+
+}
---
+++ hai/src/hai/Hello.java
@@ -0,0 +1,23 @@
+package hai;
+
+public class Hello {
+byte bt;
+	static int a=10;
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Hello h1=new Hello();
+		h1.bt=20;
+		System.out.println(h1.bt);
+		
+		char ch='g';
+		String name="sree";
+		System.out.println(ch);
+		System.out.println(name);
+		System.out.println(a);
+		
+	
+
+	}
+
+}
